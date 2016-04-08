f2d3eb3f1f3a24809275515c96ec1a00ea2b9141
Gopi Kumar <kumar91gopi@gmail.com>
2016-04-08 09:01:10 +0000

    Add Finding missing number in array prob

---
+++ FindMissingNumber.rb
@@ -0,0 +1,39 @@
+# Problem:
+=begin
+Given an array of size N ,taht has elements in range 1 to N ,all elements in the arrayb are unique, 
+Find the missing number.
+e.g.
+array = [1,5,2,4,6]
+answer = 3
+=end
+
+#Approach 1 - Find the sum of all elements of array and substract it from sum of the range , the difference will be missing number.
+# Time-complexity = O(n)
+#Caveats: If the numbers are very large the sum can lead to integer overflow.
+
+def find_missing_number(a)
+    len = a.length
+    range_sum = (len * (len+1))/2
+    arr_sum = 0
+    for i in 0...len
+        arr_sum+=a[i]
+    end
+    
+    return missing_element = range_sum - arr_sum
+    
+end
+
+
+#Approach 2 (No integer overflow)- Take bitwise XOR of all elements of the array with the numbers in given range, 
+#As( a XOR a == 0 ) all numbers present in array will xor out and become zero except the missing number. 
+# Time-complexity = O(n)
+
+def find_missing_number(a)
+    len = a.length
+    missing_element = 0
+    (1..len+1).each {|num| missing_element^= num}
+    for x in a
+        missing_element^=x
+    end
+  return missing_element
+end
